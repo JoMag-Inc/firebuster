4487db3fdf770143fdf1b3ca3e3ca58b4c71f1bc
swampbear <jobjorn.myren@gmail.com>
2026-02-15 14:08:44 +0100

    feat: added some example handlers from the FastAPI docs
    - path parameters
    - query parameters

--- app/main.py
+++ app/main.py
@@ -6,3 +6,22 @@ app = FastAPI()
 @app.get("/api/health")
 def get_health():
     return {"Status": "ok"}
+
+
+# Added example path parameter handlers from FastAPI documentation https://fastapi.tiangolo.com/tutorial/path-params/
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id", item_id}
+
+@app.get("/itemstypes/{item_id}")
+async def read_item_types(item_id: int):
+    return {"item_id", item_id}
+
+# Query parameter 
+# can be tested with `curl "http://127.0.0.1:8000/items/?skip=0&limit=10"`
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/")
+async def read_item_query(skip: int = 0, limit: int =10):
+    return fake_items_db[skip: skip+limit]
