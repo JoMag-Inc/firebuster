5741e82b04859f65fa4344724e99e85ff3290673
swampbear <jobjorn.myren@gmail.com>
2026-02-19 08:30:18 +0100

    feat: added TestClient to health test to mock endpoint and also assert status code

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,7 +1,11 @@
 import unittest
-from app.main import get_health
+from app.main import app 
+from fastapi.testclient import TestClient
+
 
 class TestHealth(unittest.TestCase):
     def test_get_health(self):
-        health = get_health()
-        self.assertEqual({"status": "ok"}, health)
+        client = TestClient(app)
+        response = client.get("/api/health")
+        self.assertEqual(200, response.status_code)
+        self.assertEqual({"status": "ok"}, response.json())
