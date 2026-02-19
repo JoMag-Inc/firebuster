1d89ae203bcc6a03aca424103bbc9aff7221e506
swampbear <jobjorn.myren@gmail.com>
2026-02-19 08:31:45 +0100

    fix: fix syntax to match json in example handlers

--- app/main.py
+++ app/main.py
@@ -11,11 +11,11 @@ def get_health():
 
 @app.get("/items/{item_id}")
 async def read_item(item_id):
-    return {"item_id", item_id}
+    return {"item_id": item_id}
 
 @app.get("/itemstypes/{item_id}")
 async def read_item_types(item_id: int):
-    return {"item_id", item_id}
+    return {"item_id": item_id}
 
 # Query parameter 
 # can be tested with `curl "http://127.0.0.1:8000/items/?skip=0&limit=10"`
