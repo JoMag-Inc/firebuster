03f0cd006d305529653270755d56c4ad43e74bf5
mag-san <162726509+mag-san@users.noreply.github.com>
2026-03-19 11:34:07 +0100

    update

--- tests/test_met.py
+++ tests/test_met.py
@@ -1,8 +1,15 @@
 import unittest
+from unittest.mock import patch, MagicMock
 from app.main import app
 from app.services.weather.weather_get_data_v2 import process_weather_data
+from app.services.weather.weather_get_data_v2 import get_weather_data
 from fastapi.testclient import TestClient
 
+lat = 21.37852609079965
+lon = 39.79370287864698
+
 class TestMet(unittest.TestCase):
-    def test_get_met(self):
-        self.assertEqual()
+    @patch('app.services.weather.weather_get_data_v2.requests')
+    def test_get_met(self, mock_requests):
+        #configure mock return value
+        mock_response = mock_requests.get.return_value
