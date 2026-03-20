80cd7ff1a0984627c226b7d03a2573814b8e02a6
mag-san <162726509+mag-san@users.noreply.github.com>
2026-03-20 08:05:53 +0100

    refactor

--- tests/test_met.py
+++ tests/test_met.py
@@ -1,8 +1,8 @@
 import unittest
 from unittest.mock import patch, MagicMock
 from app.main import app
-from app.services.weather.weather_get_data_v2 import process_weather_data
-from app.services.weather.weather_get_data_v2 import get_weather_data
+from app.services.weather.weather_get import process_weather_data
+from app.services.weather.weather_get import get_weather_data
 from fastapi.testclient import TestClient
 
 lat = 21.37852609079965
