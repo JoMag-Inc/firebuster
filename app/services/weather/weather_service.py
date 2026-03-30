7465e2b740b98f8362a74f26ef4374aa7a4d8b01
swampbear <jobjorn.myren@gmail.com>
2026-03-30 14:41:21 +0200

    feat: implement get weather at location in weather service useing the database and met api

--- app/services/weather/weather_service.py
+++ app/services/weather/weather_service.py
@@ -1,13 +1,28 @@
+import io
+import pandas as pd
 from app.repositories.weather_repository import WeatherRepository
+from app.services.weather.weather_get import (
+    get_weather_data_for_coordinates,
+    process_weather_data,
+)
 
 
 class WeatherService:
     def __init__(self) -> None:
         self.repository = WeatherRepository()
 
-    def get_weather(self):
-        weather_csv = self.repository.fetch_weather()
-        return weather_csv
+    def get_weather_at_location(self, long, lat):
+        cached_csv = self.repository.get_by_location(long, lat)
+        if cached_csv:
+            print("Cache hit! Retrieving from database")
+            return cached_csv
 
-    def save_weather_csv(self, weather_csv):
-        pass
+        print("Cache Miss! Retrieving from API")
+        data = get_weather_data_for_coordinates(long, lat)
+        csv_output = process_weather_data(data)
+        self.save_weather_csv(csv_output, long, lat)
+        return csv_output
+
+    def save_weather_csv(self, csv_output, long, lat):
+        df = pd.read_csv(io.StringIO(csv_output))
+        self.repository.add(df, long, lat)
