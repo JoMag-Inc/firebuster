8a998fd2cc29ce3104683ead0a436873fe1efe6d
swampbear <jobjorn.myren@gmail.com>
2026-03-30 10:40:08 +0200

    feat: add initial weather repository code

---
+++ app/repositories/weather_repository.py
@@ -0,0 +1,22 @@
+import psycopg2
+
+
+class WeatherRepository:
+    def fetchWeather(self):
+        conn = psycopg2.connect(
+            database="postgres",
+            user="postgres",
+            password="admin",
+            host="postgres",
+            port="5232",
+        )
+
+        cursor = conn.cursor()
+
+        cursor.execute("select * from metdata")
+
+        data = cursor.fetchall()
+        return data
+
+    def save_weather(self, data_csv):
+        pass
